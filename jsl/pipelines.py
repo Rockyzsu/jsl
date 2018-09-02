8daf7d635a55f2a52837a73fb1ebe2ba191b1b30
Rocky Chen <jinweizsu@gmail.com>
2018-09-02 21:35:17 +0800

    fix bug

--- jsl/pipelines.py
+++ jsl/pipelines.py
@@ -10,7 +10,7 @@ class JslPipeline(object):
     def __init__(self):
         self.db = pymongo.MongoClient(host='10.18.6.102',port=27018)
         # self.user = u'neo牛3' # 修改为指定的用户名 如 毛之川 ，然后找到用户的id，在用户也的源码哪里可以找到 比如持有封基是8132
-        self.collection = self.db['db_park']['jsl1']
+        self.collection = self.db['db_parker']['jsl']
     def process_item(self, item, spider):
         self.collection.insert(OrderedDict(item))
         return item
