3326046872afdad06b98fe1461c9af9192785574
Rocky Chen <jinweizsu@gmail.com>
2019-05-13 17:21:36 +0800

    完成

--- jsl/pipelines.py
+++ jsl/pipelines.py
@@ -6,17 +6,52 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymongo
 from collections import OrderedDict
+from jsl.items import Relationship,JslItem
+from jsl import config
 class JslPipeline(object):
     def __init__(self):
-        self.db = pymongo.MongoClient(host='10.18.6.26',port=27001)
+        self.db = pymongo.MongoClient(host=config.mongo,port=config.port)
         # self.user = u'neo牛3' # 修改为指定的用户名 如 毛之川 ，然后找到用户的id，在用户也的源码哪里可以找到 比如持有封基是8132
         self.collection = self.db['db_parker']['jsl']
+        self.relations = self.db['db_parker']['jsl_relationship']
 
     def process_item(self, item, spider):
-        try:
-            self.collection.update({'url':item['url']},OrderedDict(item),True,True)
-        except Exception as e:
-            print(e)
+        if isinstance(item,JslItem):
+            try:
+                self.collection.update({'url':item['url']},OrderedDict(item),True,True)
+            except Exception as e:
+                print(e)
+
+        elif isinstance(item,Relationship): # 这里会比较复杂
+            # print(item)
+            # user_id = scrapy.Field()
+            # flag = scrapy.Field()
+            # user = scrapy.Field()
+            # prestige = scrapy.Field()  # 威望
+            # approve = scrapy.Field()  # 赞同
+            # follows_count = scrapy.Field()
+            # fans_count = scrapy.Field()
+            # follows_list = scrapy.Field()
+            # fans_list = scrapy.Field()
+            # crawltime = scrapy.Field()
+
+            # 存在
+            if list(self.relations.find({'user_id':item['user_id']})):
+                if item['flag']=='follow': # 粉丝
+                    follows_list = item['follows_list']
+                    for follower in follows_list:
+                        self.relations.update({'user_id':item['user_id']},{'$push':{'follows_list':follower}})
+                else: # 关注他人
+                    fans_list = item['fans_list']
+                    for fan in fans_list:
+                        self.relations.update({'user_id': item['user_id']}, {'$push': {'fans_list': fan}})
+
+            # 不存在
+            else:
+                d=dict(item)
+                del d['flag']
+                self.relations.insert(d)
 
 
         return item
+
--- jsl/spiders/relationship.py
+++ jsl/spiders/relationship.py
@@ -2,12 +2,13 @@
 # website: http://30daydo.com
 # @Time : 2019/5/12 20:11
 # @File : relationship.py
+import datetime
 import re
 
 import math
-from scrapy.linkextractors import LinkExtractor
+# from scrapy.linkextractors import LinkExtractor
 
-from scrapy.spider import Spider, Request
+from scrapy import Spider, Request
 from jsl.items import Relationship
 
 
@@ -25,8 +26,8 @@ class RelationshipSpider(Spider):
     }
 
     people_url = 'https://www.jisilu.cn/people/{name}'
-    follow_url = 'https://www.jisilu.cn/people/ajax/follows/type-follows__uid-{user_id}__page-{page}'
-    fan_url = 'https://www.jisilu.cn/people/ajax/follows/type-fans__uid-{user_id}__page-{page}'
+    follow_url = 'https://www.jisilu.cn/people/ajax/follows/type-follows__uid-{user_id}__page-{page}' # 关注的人
+    fan_url = 'https://www.jisilu.cn/people/ajax/follows/type-fans__uid-{user_id}__page-{page}' # 粉丝
 
     def start_requests(self):
         start_user = '帅牛'
@@ -49,6 +50,9 @@ class RelationshipSpider(Spider):
         fans_num = int(
             response.xpath('//div[@class="pull-left aw-user-center-follow-mini-mod"][2]/b/em/text()').extract_first())
 
+        prestige = int(response.xpath('//i[@class="aw-icon i-user-prestige"]/following::*[1]/text()').extract_first())
+        approve = int(response.xpath('//i[@class="aw-icon i-user-approve"]/following::*[1]/text()').extract_first())
+
         user_id = re.search('var PEOPLE_USER_ID = \'(\d+)\';', response.text)
 
         if user_id:
@@ -59,32 +63,77 @@ class RelationshipSpider(Spider):
         item['follows_count'] = follows_num
         item['fans_count'] = fans_num
 
+        item['approve'] = approve
+        item['prestige'] = prestige
+
         follows_pages = int(math.ceil(follows_num / 30))
         fans_pages = int(math.ceil(fans_num / 30))
         for follow_page in range(follows_pages):
             yield Request(
-                url=self.follow_url.format(user_id=user_id,page=follow_page),
+                url=self.follow_url.format(user_id=user_id, page=follow_page),
                 headers=self.headers,
                 callback=self.follow_item,
-                meta={'item':item.copy()}
+                meta={'item': item.copy()}
             )
 
-        # for fan_page in range(fans_pages):
+        for fan_page in range(fans_pages):
+            yield Request(
+                url=self.fan_url.format(user_id=user_id, page=fan_page),
+                headers=self.headers,
+                callback=self.fan_item,
+                meta={'item': item.copy()}
+            )
 
-
-    def follow_item(self,response):
+    def follow_item(self, response):
         item = response.meta['item']
-        item['flag']='follow'
+        item['flag'] = 'follow'
 
-        follow_list =[]
+        follow_list = []
         for node in response.xpath('//li[@class="span5"]'):
-            d={}
-            follower_name = node.xpath('./div[@class="aw-item"]/p[1]/a/text()').extract_first()
-            prestige = int(node.xpath('../i[@class="aw-icon i-user-prestige"]/following::[1]/text()').extract_first())
-            d['follower_name']=follower_name
-            d['prestige']=prestige
+            d = {}
+            follower_name = node.xpath('.//div[@class="aw-item"]/p[1]/a/text()').extract_first()
+            prestige = int(node.xpath('.//i[@class="aw-icon i-user-prestige"]/following::*[1]/text()').extract_first())
+            approve = int(node.xpath('.//i[@class="aw-icon i-user-approve"]/following::*[1]/text()').extract_first())
+            d['follow_name'] = follower_name
+            d['prestige'] = prestige
+            d['approve'] = approve
             follow_list.append(d)
 
-        item['follows_list']=follow_list
-        item['crawltime']=
+            yield Request(
+                url=self.people_url.format(name=follower_name),
+                headers=self.headers,
+                meta={'user': follower_name},
+                callback=self.parse_user
+            )
 
+        item['follows_list'] = follow_list
+        item['crawltime'] = datetime.datetime.now()
+        yield item
+
+    def fan_item(self, response):
+
+        item = response.meta['item']
+        item['flag'] = 'fan'
+
+        follow_list = []
+        for node in response.xpath('//li[@class="span5"]'):
+            d = {}
+            fan_name = node.xpath('.//div[@class="aw-item"]/p[1]/a/text()').extract_first()
+            prestige = int(node.xpath('.//i[@class="aw-icon i-user-prestige"]/following::*[1]/text()').extract_first())
+            approve = int(node.xpath('.//i[@class="aw-icon i-user-approve"]/following::* [1]/text()').extract_first())
+
+            d['fan_name'] = fan_name
+            d['prestige'] = prestige
+            d['approve'] = approve
+            yield Request(
+                url=self.people_url.format(name=fan_name),
+                headers=self.headers,
+                meta={'user': fan_name},
+                callback=self.parse_user
+            )
+
+            follow_list.append(d)
+
+        item['fans_list'] = follow_list
+        item['crawltime'] = datetime.datetime.now()
+        yield item
