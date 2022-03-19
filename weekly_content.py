36a9f4d91209b48bae698b990630405cf92a4215
Rockyzsu <jinweizsu@gmail.com>
2022-03-20 00:38:34 +0800

    crawler

---
+++ weekly_content.py
@@ -0,0 +1,11 @@
+# -*-coding=utf-8-*-
+
+# @Time : 2018/12/27 16:58
+# @File : daily_content.py
+from scrapy import  cmdline
+import datetime
+# 获取指定日期内的所有帖子
+
+# cmd = 'scrapy crawl allcontent -s LOG_FILE=log/allcontent-{}.log -a daily=yes'.format(datetime.datetime.now().strftime('%Y-%m-%d'))
+cmd = 'scrapy crawl week_content'
+cmdline.execute(cmd.split())
